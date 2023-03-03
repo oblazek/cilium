c413f88dc371e85a8884c58ded8501b4414eefd5
Marco Iorio <marco.iorio@isovalent.com>
2023-03-03 17:43:21 +0100

    agent: rework clustermesh config watcher
    The agent leverages an fsnotify watcher to detect file changes in
    the directory containing the etcd configurations for remote clusters
    (as well as the associated keys/certs), triggering a reconfiguration
    as appropriate.
    
    This commit reworks the above logic to address two main issues:
    * Due to how Kubernetes mounts ConfigMaps and Secrets within pods,
      and in particular the usage of symbolic links to handle atomic
      updates, changes in existing files were not detected (since the
      watched file itself was a symbolic link, which doesn't change
      during such operation). Now, an explicit watch operation is started
      for each configuration file (i.e., watching the actual target),
      causing a notification to be properly emitted when that is changed.
    * Prevent possible spurious notifications even if the configuration
      is not actually modified. This is achieved through hash comparison.
    
    Integration tests are updated to cover the above two aspects.
    
    Fixes: #23273
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- pkg/clustermesh/config_test.go
+++ pkg/clustermesh/config_test.go
@@ -7,20 +7,24 @@ package clustermesh
 
 import (
 	"context"
+	"crypto/sha256"
 	"os"
 	"path"
 	"time"
 
 	. "gopkg.in/check.v1"
 
-	"github.com/cilium/cilium/pkg/identity/cache"
 	"github.com/cilium/cilium/pkg/ipcache"
 	"github.com/cilium/cilium/pkg/testutils"
-	testidentity "github.com/cilium/cilium/pkg/testutils/identity"
 )
 
-func createFile(c *C, name string) {
-	err := os.WriteFile(name, []byte("endpoints:\n- https://cluster1.cilium-etcd.cilium.svc:2379\n"), 0644)
+const (
+	content1 = "endpoints:\n- https://cluster1.cilium-etcd.cilium.svc:2379\n"
+	content2 = "endpoints:\n- https://cluster1.cilium-etcd.cilium.svc:2380\n"
+)
+
+func writeFile(c *C, name, content string) {
+	err := os.WriteFile(name, []byte(content), 0644)
 	c.Assert(err, IsNil)
 }
 
@@ -43,6 +47,19 @@ func expectChange(c *C, cm *ClusterMesh, name string) {
 	}
 }
 
+func expectNoChange(c *C, cm *ClusterMesh, name string) {
+	cm.mutex.RLock()
+	cluster := cm.clusters[name]
+	cm.mutex.RUnlock()
+	c.Assert(cluster, Not(IsNil))
+
+	select {
+	case <-cluster.changed:
+		c.Fatal("unexpected changed event detected")
+	case <-time.After(100 * time.Millisecond):
+	}
+}
+
 func expectNotExist(c *C, cm *ClusterMesh, name string) {
 	cm.mutex.RLock()
 	defer cm.mutex.RUnlock()
@@ -55,20 +72,22 @@ func (s *ClusterMeshTestSuite) TestWatchConfigDirectory(c *C) {
 		skipKvstoreConnection = false
 	}()
 
-	dir, err := os.MkdirTemp("", "multicluster")
+	dir1, err := os.MkdirTemp("", "multicluster-1")
 	c.Assert(err, IsNil)
-	defer os.RemoveAll(dir)
+	defer os.RemoveAll(dir1)
 
-	file1 := path.Join(dir, "cluster1")
-	file2 := path.Join(dir, "cluster2")
-	file3 := path.Join(dir, "cluster3")
+	dir2, err := os.MkdirTemp("", "multicluster-2")
+	c.Assert(err, IsNil)
+	defer os.RemoveAll(dir2)
 
-	createFile(c, file1)
-	createFile(c, file2)
+	file1 := path.Join(dir1, "cluster1")
+	file2 := path.Join(dir2, "whatever")
+	file3 := path.Join(dir1, "cluster3")
+	file2SL := path.Join(dir1, "cluster2")
 
-	mgr := cache.NewCachingIdentityAllocator(&testidentity.IdentityAllocatorOwnerMock{})
-	// The nils are only used by k8s CRD identities. We default to kvstore.
-	<-mgr.InitIdentityAllocator(nil, nil)
+	writeFile(c, file1, content1)
+	writeFile(c, file2, content1)
+	c.Assert(os.Symlink(file2, file2SL), IsNil)
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -107,7 +126,7 @@ func (s *ClusterMeshTestSuite) TestWatchConfigDirectory(c *C) {
 		return len(cm.clusters) == 1
 	}, time.Second), IsNil)
 
-	createFile(c, file3)
+	writeFile(c, file3, content1)
 
 	// wait for cluster3 to appear
 	c.Assert(testutils.WaitUntil(func() bool {
@@ -135,10 +154,12 @@ func (s *ClusterMeshTestSuite) TestWatchConfigDirectory(c *C) {
 	// touch file
 	err = os.Chtimes(file1, time.Now(), time.Now())
 	c.Assert(err, IsNil)
+	expectNoChange(c, cm, "cluster1")
 
-	// give time for events to be processed
-	time.Sleep(100 * time.Millisecond)
-	expectChange(c, cm, "cluster1")
+	// update file content
+	writeFile(c, file2, content2)
+	c.Assert(err, IsNil)
+	expectChange(c, cm, "cluster2")
 
 	err = os.RemoveAll(file1)
 	c.Assert(err, IsNil)
@@ -155,6 +176,10 @@ func (s *ClusterMeshTestSuite) TestWatchConfigDirectory(c *C) {
 	expectNotExist(c, cm, "cluster2")
 	expectNotExist(c, cm, "cluster3")
 
+	// Ensure that per-config watches are removed properly
+	wl := cm.configWatcher.watcher.WatchList()
+	c.Assert(wl, HasLen, 1)
+	c.Assert(wl[0], Equals, dir1)
 }
 
 func (s *ClusterMeshTestSuite) TestIsEtcdConfigFile(c *C) {
@@ -163,12 +188,23 @@ func (s *ClusterMeshTestSuite) TestIsEtcdConfigFile(c *C) {
 	defer os.RemoveAll(dir)
 
 	validPath := path.Join(dir, "valid")
-	err = os.WriteFile(validPath, []byte("endpoints:\n- https://cluster1.cilium-etcd.cilium.svc:2379\n"), 0644)
+	content := []byte("endpoints:\n- https://cluster1.cilium-etcd.cilium.svc:2379\n")
+	err = os.WriteFile(validPath, content, 0644)
 	c.Assert(err, IsNil)
-	c.Assert(isEtcdConfigFile(validPath), Equals, true)
+
+	isConfig, hash := isEtcdConfigFile(validPath)
+	c.Assert(isConfig, Equals, true)
+	c.Assert(hash, Equals, fhash(sha256.Sum256(content)))
 
 	invalidPath := path.Join(dir, "valid")
 	err = os.WriteFile(invalidPath, []byte("sf324kj234lkjsdvl\nwl34kj23l4k\nendpoints"), 0644)
 	c.Assert(err, IsNil)
-	c.Assert(isEtcdConfigFile(invalidPath), Equals, false)
+
+	isConfig, hash = isEtcdConfigFile(validPath)
+	c.Assert(isConfig, Equals, false)
+	c.Assert(hash, Equals, fhash{})
+
+	isConfig, hash = isEtcdConfigFile(dir)
+	c.Assert(isConfig, Equals, false)
+	c.Assert(hash, Equals, fhash{})
 }
