a2969471e6d94bcdd525ea64787b143320dc1580
Ondrej Blazek <ondrej.blazek@firma.seznam.cz>
2023-09-12 13:13:51 +0200

    fix: remove kernel proto from endpoint ip rules
    Kernel protocol routes are ignored in setups with BIRD.
    Temporarily disable this feature until configurable option is available.

--- pkg/datapath/loader/loader.go
+++ pkg/datapath/loader/loader.go
@@ -18,7 +18,6 @@ import (
 	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/byteorder"
 	"github.com/cilium/cilium/pkg/datapath/link"
-	"github.com/cilium/cilium/pkg/datapath/linux/linux_defaults"
 	"github.com/cilium/cilium/pkg/datapath/linux/route"
 	"github.com/cilium/cilium/pkg/datapath/loader/metrics"
 	datapath "github.com/cilium/cilium/pkg/datapath/types"
@@ -100,7 +99,6 @@ func upsertEndpointRoute(ep datapath.Endpoint, ip net.IPNet) error {
 		Prefix: ip,
 		Device: ep.InterfaceName(),
 		Scope:  netlink.SCOPE_LINK,
-		Proto:  linux_defaults.RTProto,
 	}
 
 	return route.Upsert(endpointRoute)
